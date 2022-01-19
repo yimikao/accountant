23d9ceae6a8057200e69dedfd1b7aa8ec708d843
yinka <holayinkajr@gmail.com>
2022-01-19 08:21:58 +0100

    finished debit command

--- cmd/debit.go
+++ cmd/debit.go
@@ -5,10 +5,22 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
+	"github.com/yimikao/accountant/database"
 )
+
+//vars to store debit flags
+var DebitAmount float64
+var DebitDetails string
+
+func checkBalance(amount float64, balance float64) string {
+	if balance-amount < 0 {
+		return "insufficient balance"
+	}
+	return ""
+}
 
 // debitCmd represents the debit command
 var debitCmd = &cobra.Command{
@@ -17,7 +29,24 @@ var debitCmd = &cobra.Command{
 	Long: `This command allows to create a debit transaction for a particular user.
 Usage: accountant debit <username> --amount=<amount> --details:<details>`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("debit called")
+
+		if len(args) < 1 {
+			log.Fatal("Username not specified.")
+		}
+
+		username := args[0]
+
+		u, err := database.FindUser(username)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		status := checkBalance(DebitAmount, u.Balance)
+		if len(status) > 0 {
+			log.Fatal(status)
+			return
+		}
+		u.Update(-DebitAmount, DebitDetails)
 	},
 }
 
@@ -33,4 +62,10 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// debitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	debitCmd.Flags().Float64VarP(&DebitAmount, "amount", "a", 0, "amount to deduct from user's balance")
+	debitCmd.Flags().StringVarP(&DebitDetails, "details", "d", "", "what the amount was deducted for.")
+
+	debitCmd.MarkFlagRequired("amount")
+	debitCmd.MarkFlagRequired("details")
 }
